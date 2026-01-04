fc91a870cd94bfe09c05aad41421c5bb74fee0e8
RUYA23 <ruyax23@gmail.com>
2026-01-04 01:10:43 +0100

    fde_momento

--- js/momentos.js
+++ js/momentos.js
@@ -3,15 +3,20 @@ document.addEventListener("DOMContentLoaded", function () {
         effect: "slide",
         loop: true,
         speed: 800,
+        allowTouchMove: true,
+        parallax: true,
+
         autoplay: {
-            delay: 3000, // Cambio cada 3 segundos
+            delay: 3000, // Pasa cada 3 segundos
             disableOnInteraction: false,
-            pauseOnMouseEnter: true, // SE PARA AL PONER EL RATÓN
+            pauseOnMouseEnter: true, // SE PARA si pones el ratón encima
         },
+
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
         },
+
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
